3329fec2689d8b52fa84bfd45eaecfb7ab9b5292
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2016-03-07 19:52:39 -0300

    Melhoria nas views do devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,14 @@ Rails.application.configure do
 
   # Devise Config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    tls: true,
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: :login,
+    user_name: "vitor.vezani@gmail.com",
+    password: "1309Viv228658"
+  }
 end
