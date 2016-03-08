b461a2d9585bbe3399879e4309f80c47afddbb17
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2016-03-08 19:27:47 -0300

    Melhorias no coupon

---
+++ config/initializers/better_errors.rb
@@ -0,0 +1,3 @@
+# e.g. in config/initializers/better_errors.rb
+# Other preset values are [:mvim, :macvim, :textmate, :txmt, :tm, :sublime, :subl, :st]
+BetterErrors.editor = :subl if defined?(BetterErrors)
