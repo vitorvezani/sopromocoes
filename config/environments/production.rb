48b2091088f5e11048d5c5589048a85cb2df6db6
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2016-03-14 22:07:38 -0300

    Adiciona host para o heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  #config.action_mailer.default_url_options = { :host => 'your.websitedomain.com' }
+  config.action_mailer.default_url_options = { :host => 'immense-earth-19457.herokuapp.com' } # TODO: Provisório
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => 587,
