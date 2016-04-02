5d0dd890c81ab5c7d371ec552e5db35c02f6f857
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2016-04-02 11:48:20 -0300

    Melhorias no layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
 
   def about
+    add_breadcrumb "Sobre", :about_path
   end
 
   def contact
+    add_breadcrumb "Contato", :contact_path
   end
 end
