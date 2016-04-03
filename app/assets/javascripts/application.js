725067bd940d6ff0352abb2defdc15e09bd86b3c
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2016-04-02 21:33:40 -0300

    Melhorias no layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,28 @@
 
 alertify.logPosition("top right");
 alertify.closeLogOnClick(true)
+
+// Custom alert
+
+$.rails.allowAction = function(element){
+    if( undefined === element.attr('data-confirm') ){
+        return true;
+    }
+
+    $.rails.showConfirmDialog(element);
+    return false;
+};
+
+$.rails.confirmed = function(element){
+    element.removeAttr('data-confirm');
+    element.trigger('click.rails');
+};
+
+$.rails.showConfirmDialog = function(element){
+    var msg = element.data('confirm');
+    alertify.confirm(msg, function(e){
+        if(e){
+            $.rails.confirmed(element);
+        }
+    })
+};
