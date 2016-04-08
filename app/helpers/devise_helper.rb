d400ae75de1e1c0c5aa447c199514276613c5307
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2016-04-07 22:41:39 -0300

    Melhorias na página do usuário

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -16,4 +16,14 @@ module DeviseHelper
   def is_admin?
     current_user ? current_user.admin? : false
   end
+
+  def is_logged_user_other_user(other_user)
+    return false if other_user.nil? or current_user.nil?
+    current_user.id.to_s == other_user.id.to_s
+  end
+
+  def owns_record(record)
+    return false if record.user.nil? or current_user.nil?
+    current_user.id.to_s == record.user.id.to_s
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,2 @@
 module UsersHelper
-
-  def is_logged_user(other_user)
-    return false if other_user.nil? or current_user.nil?
-    current_user.id.to_s == other_user.id.to_s
-  end
-
-  def owns_record(record)
-    return false if record.user.nil? or current_user.nil?
-    current_user.id.to_s == record.user.id.to_s
-  end
 end
